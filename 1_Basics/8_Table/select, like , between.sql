7bf42280c64f5b5b7fc73769aa8879ed6d63b57f
kushalshrestha <kushalshr@gmail.com>
2023-07-06 15:31:14 -0500

    feat: select, like, between, year filter

--- 1_Basics/8_Table/select, like , between.sql
+++ 1_Basics/8_Table/select, like , between.sql
@@ -1,3 +1,4 @@
+use [AdventureWorks2012]
 --- LIKE OPERATOR ---
 SELECT *
 FROM tblEmployee
@@ -42,3 +43,14 @@ WHERE EmployeeNumber BETWEEN 132 AND 133; -- 132 and 133 both is included
 SELECT *
 FROM tblEmployee
 WHERE EmployeeNumber NOT BETWEEN 132 AND 133; -- 132 and 133 both is included
+
+
+---- filtering date -----
+select * 
+from [AdventureWorks2012].[dbo].[tblEmployee]
+where DateofBirth > '1991' and DateofBirth < '1995';
+
+select EmployeeFirstName 
+from [AdventureWorks2012].[dbo].[tblEmployee]
+WHERE year(DateofBirth) BETWEEN '1991' and '1994'; 
+-- DO NOT USE but the reason for how it works? here not 1995 b/c between limits are inclusive
