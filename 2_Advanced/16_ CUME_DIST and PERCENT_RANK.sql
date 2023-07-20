3e6e23c40ade29471e6d012496e8cfe10fd9a7fd
kushalshrestha <kushalshr@gmail.com>
2023-07-20 01:58:48 -0500

    feat: CUME_DIST and PERCENT_RANK

---
+++ 2_Advanced/16_ CUME_DIST and PERCENT_RANK.sql
@@ -0,0 +1,11 @@
+select A.EmployeeNumber, A.AttendanceMonth, A.NumberAttendance
+, CUME_DIST() over (partition by E.EmployeeNumber order by A.AttendanceMonth) as CumulativeDistribution
+, cast(ROW_NUMBER() over (partition by E.EmployeeNumber order by A.AttendanceMonth) as decimal(9,5)) / count(*) over (partition by E.EmployeeNumber) as SelfCalculatedCumulativeDistribution
+
+
+, PERCENT_RANK() over (partition by E.EmployeeNumber order by A.AttendanceMonth) as PERCENT_RANK
+, cast(ROW_NUMBER() over (partition by E.EmployeeNumber order by A.AttendanceMonth) - 1 as decimal(9,5)) / count(*) over (partition by E.EmployeeNumber) as SelfCalculatedCumulativeDistribution
+
+from tblEmployee as E
+join tblAttendance as A
+on E.EmployeeNumber = A.EmployeeNumber
