468b84413d0bf0d91cf9933c0c5e9b8195c00f56
kushalshrestha <kushalshr@gmail.com>
2023-07-27 22:10:49 -0500

    GUIDs and Sequences demo

--- 12_GUIDs and Sequences/3_SEQUENCES.sql
+++ 12_GUIDs and Sequences/3_SEQUENCES.sql
@@ -1,16 +1,35 @@
-BEGIN Transaction
-create SEQUENCE newSeq as BIGINT
-start with 1
-increment by 1
+BEGIN TRANSACTION
+CREATE SEQUENCE newSeq AS BIGINT
+start WITH 1
+increment BY 1
 minvalue 1
 maxvalue 999999 
 cycle    -- cycle/nocycle -> reuse 
-cache 50  -- cache set of 50 values
+cache 50
+-- cache set of 50 values
 
 -- simpler way
-create sequence secondSeq as int
+CREATE SEQUENCE secondSeq AS int
 
 
 -- check sequences created
-select * from sys.sequences
-Rollback transaction
+SELECT *
+FROM sys.sequences
+
+------
+
+--USING SEQUENCES 
+-- 1st method
+SELECT NEXT VALUE FOR newSeq AS NextValue
+-- 2nd method
+select *, NEXT VALUE FOR newSeq over (order by DateOfTransaction) as NextNumber
+from tblTransaction
+
+-- USING Sequences in Table Schema
+Alter table tblTransaction
+Add NextNumber int
+Alter table tblTransaction
+add CONSTRAINT DF_Transaction DEFAULT NEXT VALUE FOR newSeq for NextNumber
+
+ROLLBACK TRANSACTION
+
--- 7_With statement/3_Grouping numbers.sql
+++ 7_With statement/3_Grouping numbers.sql
@@ -25,3 +25,27 @@ left join tblTransaction as T
 on U.RowNumber = T.EmployeeNumber
 where T.EmployeeNumber is null
 order by U.RowNumber;
+
+
+/*
+Q. IN 2014 only -> Find the list of all employees who doesn't have their EmployeeNumber in the tblTransaction
+*/
+
+with Numbers as (
+    select top(
+            select max(EmployeeNumber)
+            from tblTransaction
+            )
+            ROW_NUMBER() over (order by (select null)) as RowNumber
+    from tblTransaction as U
+),
+Transaction2014 as (select * from tblTransaction where DateOfTransaction >= '2014-01-01' and DateOfTransaction < '2015-01-01')
+
+select U.RowNumber 
+from Numbers as U
+left join Transaction2014 as T
+on U.RowNumber = T.EmployeeNumber
+where T.EmployeeNumber is null
+order by U.RowNumber;
+
+-- This is perfect but gives a lot of rows. I need to have grouped from 172 - 177. how will you do?
